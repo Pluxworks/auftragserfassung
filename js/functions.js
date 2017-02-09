1a96db5321f723138c1e01f8ebdb4c400cb96cb9
Pluxworks <m.wollmeiner@icloud.com>
2017-02-09 17:53:50 +0100

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -3,4 +3,11 @@
 $(function(){
 	$('#date').datepicker('language','de');
 	
+	$("input#aufwand").focusout(function() {
+		if($('input#aufwand').value() == '') {
+		$('#test').addClass("has-error");
+	}
+	});
+	
+	
 });
