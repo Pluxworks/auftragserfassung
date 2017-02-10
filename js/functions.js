41e482bc7c27a383c655db2ffdf52ad4211a2f6e
Pluxworks <m.wollmeiner@icloud.com>
2017-02-10 13:19:05 +0100

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -3,11 +3,33 @@
 $(function(){
 	$('#date').datepicker('language','de');
 	
-	$("input#aufwand").focusout(function() {
-		if($('input#aufwand').value() == '') {
+	$( "input#aufwand" ).focusout(function() {
+		
+		if($('input#aufwand').val() == "") {
 		$('#test').addClass("has-error");
+		
+	} else {
+		$('#test').removeClass("has-error");
 	}
+	});
+	
+	$( "li#atabelle" ).click(function() {
+		$('li#atabelle').addClass("active");
+		$('li#aeintragen').removeClass("active");
+		reload_with_tableAufwand();
+		
+	});
+	
+	$( "li#aeintragen" ).click(function() {
+		$('li#aeintragen').addClass("active");
+		$('li#atabelle').removeClass("active");
 	});
 	
 	
 });
+
+function reload_with_tableAufwand(){
+		$.get("php/tableAufwand.php", function(data) {
+        $("div#content").html(data);
+		});
+}
