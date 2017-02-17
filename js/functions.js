d43a4edc64b6489a29c37fbb3bef3d93c707f31b
Pluxworks <m.wollmeiner@icloud.com>
2017-02-18 00:04:53 +0100

    Add files via upload

--- js/functions.js
+++ js/functions.js
@@ -23,8 +23,16 @@ $(function(){
 	$( "li#aeintragen" ).click(function() {
 		$('li#aeintragen').addClass("active");
 		$('li#atabelle').removeClass("active");
+		reload_with_formAufwand();
 	});
 	
+	$( "#aufwandsaveButton" ).click(function() {
+		saveAufwand();
+	});
+	
+	$( "#back" ).click(function() {
+		back();
+	});
 	
 });
 
@@ -32,4 +40,33 @@ function reload_with_tableAufwand(){
 		$.get("php/tableAufwand.php", function(data) {
         $("div#content").html(data);
 		});
+}
+
+function reload_with_formAufwand(){
+		$.get("php/formAufwand.php", function(data) {
+        $("div#content").html(data);
+		});
+}
+
+function saveAufwand() {
+	$.ajax ({
+		type: "POST",
+		url: "ajax/saveAufwand.php",
+		data: {
+			datum: $( "div#content input#date" ).val(),
+			bereich_id: $( "div#content select#bereich option:selected" ).val(),
+			kunde_id: $( "div#content select#kunde option:selected" ).val(),
+			vertrag: $( "div#content select#vertrag option:selected" ).val(),
+			aufwand: $( "div#content input#aufwand" ).val(),
+			mitarbeiter_id: $( "div#content select#mitarbeiter option:selected" ).val(),
+			taetigkeit: $( "div#content textarea#taetigkeit" ).val(),
+			bemerkung: $( "div#content textarea#bemerkung" ).val()
+		}
+	}).done(function(data) {
+		$("div#content").html(data);
+	});
+}
+
+function back() {
+	window.location.href = "./";
 }
